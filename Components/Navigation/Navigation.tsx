57a76022f047d9cf5fe782b52eaa077848f3a8b4
GrigArakelyan <araqelyan461@gmail.com>
2023-10-10 19:29:50 +0400

    fix project

--- Components/Navigation/Navigation.tsx
+++ Components/Navigation/Navigation.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 import Link from "next/link"
 import Style from './navigation.module.scss'
 import { ABOUT, HOME, NEWSLETTER, PRICING, ROOMS, SERVICES } from "@/constants/rout"
-import { HoteLogo } from "@/constants/public"
+import { HeaderLogo } from "@/constants/public"
 
 const Navigation = () => {
 
@@ -14,7 +14,7 @@ const Navigation = () => {
          <Link href={HOME} className={isActive === HOME ? Style.active : Style.navLink}>Home</Link>
          <Link href={ABOUT} className={isActive === ABOUT ? Style.active : Style.navLink}>About</Link>
          <Link href={NEWSLETTER} className={isActive === NEWSLETTER ? Style.active : Style.navLink}>Newsletter</Link>
-         <Image src={HoteLogo} alt="" width={78} height={42} className={Style.headerLogo}/>
+         <Image src={HeaderLogo} alt="" width={70} height={42} className={Style.headerLogo}/>
          <Link href={ROOMS} className={isActive === ROOMS ? Style.active : Style.navLink}>Rooms</Link>
          <Link href={SERVICES} className={isActive === SERVICES ? Style.active : Style.navLink}>Services</Link>
          <Link href={PRICING} className={isActive === PRICING ? Style.active : Style.navLink}>Pricing</Link>
--- constants/public.ts
+++ constants/public.ts
@@ -1,3 +1,4 @@
 export const HoteLogo = "/Logo.svg";
+export const HeaderLogo = "/Logo.png";
 export const HeaderImageOne = "/Image.png";
 export const HeaderSlideDot = '/Ellipse.svg';
